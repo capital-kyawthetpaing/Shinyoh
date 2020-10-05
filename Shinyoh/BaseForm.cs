ef3119be98d11669dc99ab10e80fab43459b708c
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2020-10-05 11:18:04 +0630

    Add project files.

---
+++ Shinyoh/BaseForm.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using CKM_CommonFunction;
+using Newtonsoft.Json;
+
+namespace Shinyoh
+{
+    public partial class BaseForm : Form
+    {
+        #region Login Information
+        protected string CompanyCD { get; set; }
+        protected string OperatorCD { get; set; }
+        protected string PCID { get; set; }
+        #endregion
+
+        CKMFunction ckmfun;
+
+        public BaseForm()
+        {
+            InitializeComponent();
+            ckmfun = new CKMFunction();
+
+        }
+
+        protected void StartProgram()
+        {
+            string filePath = string.Empty;
+            if (Debugger.IsAttached)
+            {
+                System.Uri u = new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+                filePath = System.IO.Path.GetDirectoryName(u.LocalPath) + @"\" + "CKM.ini";
+            }
+            else
+            {
+                filePath = @"C:\\DBConfig\\CKM.ini";
+            }
+            Dictionary<string, string> dicConfig = ckmfun.ReadConfig(filePath, "DataBase", "ShinyohDB");
+            if (this.GetCmdLine() == false || dicConfig.Count == 0)
+            {
+                //起動時エラー    DB接続不可能
+                this.Close();
+                System.Environment.Exit(0);
+            }
+
+
+        }
+
+        private bool GetCmdLine()
+        {
+            string[] cmds = System.Environment.GetCommandLineArgs();
+
+            if (cmds.Length < 3)
+                return false;
+
+            CompanyCD = cmds[0];
+            OperatorCD = cmds[1];
+            PCID = cmds[2];
+
+            if (string.IsNullOrWhiteSpace(CompanyCD)
+                || string.IsNullOrWhiteSpace(OperatorCD)
+                || string.IsNullOrWhiteSpace(PCID))
+                return false;
+
+            return true;
+        }
+    }
+}
