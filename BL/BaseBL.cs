ebaf2e38383317e824bb5505bd1581c77335eaae
kyawthetpaing <kyawthetpaing@mm.gmail.com>
2020-10-05 13:08:41 +0630

    ktp

--- BL/BaseBL.cs
+++ BL/BaseBL.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BL
+﻿namespace BL
 {
-    class BaseBL
+    public class BaseBL
     {
 
     }
