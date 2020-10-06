7e18a3031267d0e6ac59ac40aa2b7de9072dabd3
nwemar <capital.nwemarwin@gmail.com>
2020-10-06 11:35:17 +0630

    NMW(6-10-2020)

---
+++ Shinyoh_Controls/LBox_Buff.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace Shinyoh_Controls
+{
+   public  class LBox_Buff : Label
+    {
+        public LBox_Buff()
+        {
+            BackColor = Color.FromArgb(255, 230, 153);
+            AutoSize = false;
+            TextAlign = ContentAlignment.MiddleCenter;
+            BorderStyle = BorderStyle.None;
+            MinimumSize = new Size(100, 25);
+            FlatStyle = FlatStyle.Flat;
+            Font = new Font(Label.DefaultFont, FontStyle.Bold);
+        }
+    }
+}
---
+++ Shinyoh_Controls/LBox_Red.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace Shinyoh_Controls
+{
+    public class LBox_Red : Label
+    {
+        public LBox_Red()
+        {
+            BackColor = Color.Red;
+            AutoSize = false;
+            TextAlign = ContentAlignment.MiddleCenter;
+            BorderStyle = BorderStyle.None;
+            MinimumSize = new Size(100, 25);
+            FlatStyle = FlatStyle.Flat;
+            Font = new Font(Label.DefaultFont, FontStyle.Bold);
+            ForeColor = Color.White;
+        }
+    }
+}
