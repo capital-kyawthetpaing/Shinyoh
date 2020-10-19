0c5de98ebb8c3929fb1c505f795673fd82de89e6
Kyaw Thet Paing <kyawthetpaing.mm@gmail.com>
2020-10-19 16:36:48 +0630

    ktp

---
+++ Shinyoh_Controls/CheckType.cs
@@ -0,0 +1,8 @@
+﻿namespace Shinyoh_Controls
+{
+    public class CheckType
+    {
+        public bool RequireCheck { get; set; }
+        public bool ZipCDCheck { get; set; }
+    }
+}
---
+++ Shinyoh_Controls/ErrorCheck.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Forms;
+
+namespace Shinyoh_Controls
+{
+    public class ErrorCheck
+    {
+        public string Check(Control ctrl)
+        {
+            if(ctrl is STextBox)
+            {
+                STextBox sTextBox = ctrl as STextBox;
+                return TextBoxErrorCheck(sTextBox);
+            }
+
+            return "0";
+        }
+
+        private string TextBoxErrorCheck(STextBox sTextBox)
+        {
+            if(sTextBox.IsRequireCheck)
+            {
+                if (!string.IsNullOrWhiteSpace(sTextBox.Text))
+                    return "E102";
+            }
+
+            if(sTextBox.IsZipCheck)
+            {
+                if(string.IsNullOrWhiteSpace(sTextBox.ctrlZip1.Text) && !string.IsNullOrWhiteSpace(sTextBox.ctrlZip2.Text))
+                {
+                    sTextBox.ctrlZip1.Focus();
+                    return "E102";
+                }
+                else if(!string.IsNullOrWhiteSpace(sTextBox.ctrlZip1.Text) && string.IsNullOrWhiteSpace(sTextBox.ctrlZip2.Text))
+                {
+                    sTextBox.ctrlZip2.Focus();
+                    return "E102";
+                }
+            }
+
+            return "0";
+        }
+    }
+}
