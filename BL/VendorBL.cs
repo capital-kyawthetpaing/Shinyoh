621e2e23faac9c5f0bf1aa71af6e71eb93663b37
nwemar <capital.nwemarwin@gmail.com>
2020-11-06 15:58:35 +0630

    NMW

--- BL/VendorBL.cs
+++ BL/VendorBL.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entity
+namespace BL
 {
-    public class SupplierEntity
+    public class VendorBL
     {
     }
 }
--- Entity/VendorEntity.cs
+++ Entity/VendorEntity.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BL
+namespace Entity
 {
-    public class SupplierBL
+    public class VendorEntity
     {
     }
 }
