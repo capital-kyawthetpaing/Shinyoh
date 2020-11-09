3a6ab7fac069fe908e00a1a87a23a4ea75d1f2c5
nwemar <capital.nwemarwin@gmail.com>
2020-11-09 10:15:43 +0630

    NMW

--- Shinyoh_Controls/SRadio.cs
+++ Shinyoh_Controls/SRadio.cs
@@ -38,5 +38,31 @@ namespace Shinyoh_Controls
                 base.OnKeyDown(e);
             }
         }
+        protected override void OnEnter(EventArgs e)
+        {
+            this.BackColor = Color.Cyan;
+            base.OnEnter(e);
+        }
+        protected override void OnLeave(EventArgs e)
+        {
+            this.BackColor = SystemColors.Menu;
+            base.OnLeave(e);
+        }
+        protected override void OnEnabledChanged(EventArgs e)
+        {
+            if (!Enabled)
+                this.BackColor = Color.FromArgb(255, 230, 153);
+            else
+                this.BackColor = SystemColors.Window;
+
+            base.OnEnabledChanged(e);
+        }
+
+        protected override void InitLayout()
+        {
+            base.InitLayout();
+            base.AutoSize = false;
+            base.Height = 19;
+        }
     }
 }
