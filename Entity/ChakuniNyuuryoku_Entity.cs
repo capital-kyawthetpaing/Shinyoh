0fe0b78aeb31d0e72219337aa55b16ba0e3fbb66
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2020-11-11 17:36:19 +0630

    by SES

---
+++ Entity/ChakuniNyuuryoku_Entity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class ChakuniNyuuryoku_Entity : BaseEntity
+    {
+        public string ChakuniNO { get; set; }
+        public string ChakuniDate { get; set; }
+        public string ChakuniDateFrom { get; set; }
+        public string ChakuniDateTo { get; set; }
+        public string SiiresakiCD { get; set; }
+        public string SiiresakiRyakuName { get; set; }
+        public string SiiresakiName { get; set; }
+        public string SiiresakiYuubinNO1 { get; set; }
+        public string SiiresakiYuubinNO2 { get; set; }
+        public string SiiresakiJuusho1 { get; set; }
+        public string SiiresakiJuusho2 { get; set; }
+        public string SiiresakiTelNO11 { get; set; }
+        public string SiiresakiTelNO12 {get;set;}
+        public string SiiresakiTelNO13 { get; set; }
+        public string SiiresakiTelNO21 { get; set; }
+        public string SiiresakiTelNO22 { get; set; }
+        public string SiiresakiTelNO23 { get; set; }
+        public string StaffCD { get; set; }
+        public string SoukoCD { get; set; }
+        public string ShouhinCD { get; set; }
+        public string ShouhinName { get; set; }
+        public string ColorRyakuName { get; set; }
+        public string ColorNO { get; set; }
+        public string SizeNO { get; set; }
+        public string ChakuniYoteiDate { get; set; }
+        public string ChakuniYoteiSuu { get; set; }
+        public string ChakuniZumiSuu { get; set; }
+        public string ChakuniSuu { get; set; }
+        public string ChakuniMeisaiTekiyou { get; set; }
+        public string JANCD { get; set; }
+        public string HacchuuNO { get; set; }
+        public string ChakuniYoteiGyouNO { get; set; }
+        public string HacchuuGyouNO { get; set; }
+        public string ChakuniYoteiDateFrom { get; set; }
+        public string ChakuniYoteiDateTo { get; set; }
+        public string KanriNOFrom { get; set; }
+        public string KanriNOTo { get; set; }
+        public string ShouhinCDFrom { get; set; }
+        public string ShouhinCDTo { get; set; }
+    }
+    
+}
