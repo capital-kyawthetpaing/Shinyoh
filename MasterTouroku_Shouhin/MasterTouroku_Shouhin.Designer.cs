74698219e38591ad2973565962dd0c4a002ef337
Than Zaw Aung <capital.thanzawaung@gmail.com>
2020-11-12 14:29:24 +0630

    tza

---
+++ MasterTouroku_Shouhin/MasterTouroku_Shouhin.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MasterTouroku_Shouhin
+{
+    partial class MasterTouroku_Shouhin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Size = new System.Drawing.Size(1485, 75);
+            // 
+            // cboMode
+            // 
+            this.cboMode.BackColor = System.Drawing.Color.Cyan;
+            // 
+            // MasterTouroku_Shouhin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1485, 504);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "MasterTouroku_Shouhin";
+            this.Text = "商品マスタ";
+            this.Load += new System.EventHandler(this.MasterTouroku_Shouhin_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
