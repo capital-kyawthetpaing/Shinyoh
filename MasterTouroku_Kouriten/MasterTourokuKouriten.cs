a80abaad3db4007bc31db10eabd1750d30759613
nwemar <capital.nwemarwin@gmail.com>
2020-11-17 12:58:38 +0630

    NMW

---
+++ MasterTouroku_Kouriten/MasterTourokuKouriten.cs
@@ -0,0 +1,30 @@
+﻿using Entity;
+using Shinyoh;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterTouroku_Kouriten
+{
+    public partial class MasterTourokuKouriten : BaseForm
+    {
+        multipurposeEntity multi_Entity ;
+        public MasterTourokuKouriten()
+        {
+            InitializeComponent();
+        }
+
+        private void MasterTourokuKouriten_Load(object sender, EventArgs e)
+        {
+            ProgramID = "MasterTourokuKouriten";
+            StartProgram();
+            cboMode.Bind(false, multi_Entity);
+        }
+    }
+}
