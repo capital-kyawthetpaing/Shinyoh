2e7c3b29381541234a6f627705fc9f4672f5d8f0
capital-hnineithu <capital.hnineithu@gmail.com>
2020-11-23 08:13:07 +0630

    byeithu

---
+++ Entity/JuchuuEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity {
+   public class JuchuuEntity : BaseEntity{      
+        public string JuhuuDate1 { get; set; }
+        public string JuhuuDate2 { get; set; }
+        public string JuhuuNO1 { get; set; }
+        public string JuhuuNO2 { get; set; }
+        public string InputDate1 { get; set; }
+        public string InputDate2 { get; set; }
+        public string StaffCD { get; set; }
+        public string BrandCD { get; set; }
+        public string TokuisakiCD { get; set; }
+        public string Year { get; set; }
+        public string SS { get; set; }
+        public string FW { get; set; }
+        public string Store { get; set; }
+        public string DestOrderNo { get; set; }
+    }
+}
