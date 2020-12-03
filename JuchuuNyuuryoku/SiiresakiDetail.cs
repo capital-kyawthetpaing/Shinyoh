e984f6cd2ce573b9f02b463359e8d6d0b3e8b2fb
nwemar <capital.nwemarwin@gmail.com>
2020-12-03 15:31:57 +0630

    NMW

--- JuchuuNyuuryoku/SiiresakiDetail.cs
+++ JuchuuNyuuryoku/SiiresakiDetail.cs
@@ -14,6 +14,7 @@ namespace JuchuuNyuuryoku
 {
     public partial class SiiresakiDetail : SearchBase
     {
+        public SiiresakiEntity Access_Siiresaki_obj = new SiiresakiEntity();
         public SiiresakiDetail()
         {
             InitializeComponent();
@@ -31,6 +32,26 @@ namespace JuchuuNyuuryoku
             txtLong_Name.E102Check(true);
             txtYubin2.E102MultiCheck(true, txtYubin1, txtYubin2);
             txtYubin2.Yuubin_Juusho(true, txtYubin1, txtYubin2, string.Empty, string.Empty);
+
+            //Get Data from JuchuuNyuuroku form
+            Access_DB_Object(Access_Siiresaki_obj);
+        }
+        private void Access_DB_Object(SiiresakiEntity obj)
+        {
+            txtCD.Text = obj.SiiresakiCD;
+            txtLong_Name.Text = obj.SiiresakiName;
+            txtShort_Name.Text = obj.SiiresakiRyakuName;
+            lbl_Name.Text = obj.SiiresakiRyakuName;
+            txtYubin1.Text = obj.YuubinNO1;
+            txtYubin2.Text = obj.YuubinNO2;
+            txtAddress1.Text = obj.Juusho1;
+            txtAddress2.Text = obj.Juusho2;
+            txtPhone1_1.Text = obj.Tel11;
+            txtPhone1_2.Text = obj.Tel12;
+            txtPhone1_3.Text = obj.Tel13;
+            txtPhone2_1.Text = obj.Tel21;
+            txtPhone2_2.Text = obj.Tel22;
+            txtPhone2_3.Text = obj.Tel23;
         }
     }
 }
