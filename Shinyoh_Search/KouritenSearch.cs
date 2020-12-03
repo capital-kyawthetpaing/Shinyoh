b099e0d6f5fe44e5f4ffe9c53c0ad41aa42aaea7
nwemar <capital.nwemarwin@gmail.com>
2020-12-03 09:14:38 +0630

    NMW

--- Shinyoh_Search/KouritenSearch.cs
+++ Shinyoh_Search/KouritenSearch.cs
@@ -64,7 +64,7 @@ namespace Shinyoh_Search
             DataTable dt = objMethod.Kouriten_Search(obj);
             if (dt.Columns.Contains("CurrentDay"))
             {
-                if(dt.Rows.Count>0)
+                if (dt.Rows.Count > 0)
                 {
                     lbl_Date.Text = String.Format("{0:yyyy/MM/dd}", dt.Rows[0]["CurrentDay"]);
                     dt.Columns.Remove("CurrentDay");
