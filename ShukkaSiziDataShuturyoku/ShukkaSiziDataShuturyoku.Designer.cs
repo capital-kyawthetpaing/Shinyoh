9893320b316cab89238f3f47877cb8adf8913a3f
capital-hnineithu <capital.hnineithu@gmail.com>
2020-12-07 13:07:35 +0630

    byeithu

---
+++ ShukkaSiziDataShuturyoku/ShukkaSiziDataShuturyoku.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ShukkaSiziDataShuturyoku {
+    partial class ShukkaSiziDataShuturyoku {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Size = new System.Drawing.Size(1468, 75);
+            // 
+            // cboMode
+            // 
+            this.cboMode.BackColor = System.Drawing.Color.Cyan;
+            // 
+            // ShukkaSiziDataShuturyoku
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1468, 479);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "ShukkaSiziDataShuturyoku";
+            this.Text = "出荷指示データ出力\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t";
+            this.Load += new System.EventHandler(this.ShukkaSiziDataShuturyoku_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
