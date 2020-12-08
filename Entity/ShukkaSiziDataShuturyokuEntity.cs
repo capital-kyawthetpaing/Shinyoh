2d517d329366d5a63b5696c1937eba1a5154e1ce
capital-hnineithu <capital.hnineithu@gmail.com>
2020-12-08 09:50:17 +0630

    byeithu

---
+++ Entity/ShukkaSiziDataShuturyokuEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity {
+   public class ShukkaSiziDataShuturyokuEntity: BaseEntity {
+        public string ShukkaNo1 { get; set; }
+        public string ShukkaNo2 { get; set; }
+        public string ShukkaDate1 { get; set; }
+        public string ShukkaDate2 { get; set; }
+        public string InputDate1 { get; set; }
+        public string InputDate2 { get; set; }
+        public string BrandCD { get; set; }
+        public string Year { get; set; }
+        public string SS { get; set; }
+        public string FW { get; set; }
+        public string TokuisakiCD { get; set; }
+        public string KouritenCD { get; set; }
+        public string Condition { get; set; }
+    }
+}
