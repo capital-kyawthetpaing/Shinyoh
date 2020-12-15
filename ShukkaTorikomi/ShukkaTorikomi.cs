99668abb95f9590f9d02025c25e153697e2b5d61
KhinMyoMyatThaw <capital.khinmyomyatthaw@gmail.com>
2020-12-15 10:10:39 +0630

    By Thaw Thaw

--- ShukkaTorikomi/ShukkaTorikomi.cs
+++ ShukkaTorikomi/ShukkaTorikomi.cs
@@ -51,28 +51,11 @@ namespace ShukkaTorikomi
 
             txtDate1.Enabled = false;
             txtDate2.Enabled = false;
+            txtNo.Enabled = false;
             dataBind();
            
         }
 
-        //private void ShukkaTorikomi_Load(object sender, KeyEventArgs e)
-        //{
-        //    multipurposeBL bl = new multipurposeBL();
-
-        //    DataTable dt = bl.M_Multiporpose_SelectData(string.Empty, 3, string.Empty, string.Empty);
-
-        //    if (dt.Rows.Count > 0)
-        //    {
-        //        txtShukkaToNo1.Text = dt.Rows[0]["Char1"].ToString();
-        //        txtShukkaToNo2.Text = dt.Rows[0]["Char2"].ToString();
-        //    }
-        //    else
-        //    {
-        //        txtShukkaToNo1.Text = string.Empty;
-        //        txtShukkaToNo2.Text = string.Empty;
-        //    }
-
-        //}
 
         private void dataBind()
         {
@@ -134,41 +117,5 @@ namespace ShukkaTorikomi
             }
         }
 
-
-
-
-
-
-        //private void ChangeMode(Mode mode)
-        //{
-        //    switch (mode)
-        //    {
-        //        case Mode.New:
-        //            ErrorCheck();
-
-        //            cf.Clear(PanelTitle);
-        //            cf.Clear(panelDetails);
-        //            cf.EnablePanel(PanelTitle);
-        //            cf.EnablePanel(panelDetails);
-        //            sbShippingNO.Focus();
-        //            New_Mode();
-        //            Control btnNew = this.TopLevelControl.Controls.Find("BtnF12", true)[0];
-        //            btnNew.Visible = true;
-        //            break;
-        //        case Mode.Update:
-        //            Control btnUpdate = this.TopLevelControl.Controls.Find("BtnF12", true)[0];
-        //            btnUpdate.Visible = true;
-        //            break;
-        //        case Mode.Delete:
-        //            Control btnDelete = this.TopLevelControl.Controls.Find("BtnF12", true)[0];
-        //            btnDelete.Visible = true;
-        //            break;
-        //        case Mode.Inquiry:
-        //            Control btnInquiry = this.TopLevelControl.Controls.Find("BtnF12", true)[0];
-        //            btnInquiry.Visible = false;
-        //            break;
-        //    }
-
-        //}
     }
 }
