64aac1bdee3d92c381cbe742db44267a51a04a1a
Kyaw Thet Paing <capital.kyawthetpaing@gmail.com>
2020-12-23 09:25:08 +0630

    ktp

--- Shinyoh_Controls/SGridView.cs
+++ Shinyoh_Controls/SGridView.cs
@@ -11,7 +11,7 @@ namespace Shinyoh_Controls
     public class SGridView : DataGridView
     {
         bool UseRow = true;
-        
+
         public void UseRowNo(bool val)
         {
             UseRow = RowHeadersVisible = val;
@@ -67,6 +67,28 @@ namespace Shinyoh_Controls
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        protected override bool ProcessCmdKey(ref System.Windows.Forms.Message msg, System.Windows.Forms.Keys keyData)
+        {
+            int icolumn = this.CurrentCell.ColumnIndex;
+            int irow = this.CurrentCell.RowIndex;
+
+            if (keyData == Keys.Enter)
+            {
+                if (icolumn == this.Columns.Count - 1)
+                {
+                    this.Rows.Add();
+                    this.CurrentCell = this[0, irow + 1];
+                }
+                else
+                {
+                    this.CurrentCell = this[icolumn + 1, irow];
+                }
+                return true;
+            }
+            else
+                return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
