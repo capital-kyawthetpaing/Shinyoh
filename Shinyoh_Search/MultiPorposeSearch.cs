d1d10188cfa2bd349c66c238541b32555fabd208
Shwe Eain San <Shwe Eain San@DESKTOP-73GSVF4>
2021-01-07 14:56:04 +0630

    by ses

--- Shinyoh_Search/MultiPorposeSearch.cs
+++ Shinyoh_Search/MultiPorposeSearch.cs
@@ -24,8 +24,6 @@ namespace Shinyoh_Search
         public MultiPorposeSearch()
         {
             InitializeComponent();
-            gvMultiporpose.SetGridDesign();
-            gvMultiporpose.SetReadOnlyColumn("*");
         }
 
         private void MultiPorposeSearch_Load(object sender, EventArgs e)
@@ -38,6 +36,7 @@ namespace Shinyoh_Search
             txtID2.E106Check(true, txtID1, txtID2);
             txtKey2.E106Check(true, txtKey1, txtKey2);
             gvMultiporpose.Select();
+            gvMultiporpose.SetGridDesign();
             gvMultiporpose.SetReadOnlyColumn("**");//readonly for search form 
             txtID1.Focus();
         }
