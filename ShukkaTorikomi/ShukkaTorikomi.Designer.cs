238915e08b7759eb96680d53ea19c2e8bf991f37
capital-hnineithu <capital.hnineithu@gmail.com>
2021-03-30 11:44:04 +0630

    byeithu

--- ShukkaTorikomi/ShukkaTorikomi.Designer.cs
+++ ShukkaTorikomi/ShukkaTorikomi.Designer.cs
@@ -59,10 +59,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.gvMainDetail)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
-            // 
-            this.panel1.Size = new System.Drawing.Size(1370, 75);
-            // 
             // PanelTitle
             // 
             this.PanelTitle.Controls.Add(this.rdo_Sakujo);
@@ -136,7 +132,7 @@
             this.PanelDetail.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PanelDetail.Location = new System.Drawing.Point(0, 75);
             this.PanelDetail.Name = "PanelDetail";
-            this.PanelDetail.Size = new System.Drawing.Size(1370, 630);
+            this.PanelDetail.Size = new System.Drawing.Size(1713, 842);
             this.PanelDetail.TabIndex = 2;
             // 
             // txtDate2
@@ -419,7 +415,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.ClientSize = new System.Drawing.Size(1713, 961);
             this.Controls.Add(this.PanelDetail);
             this.Location = new System.Drawing.Point(0, 0);
             this.Name = "SqlDbType";
