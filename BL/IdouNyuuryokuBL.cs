3387a9b510e6a5180190b6bd64c25954e6e5cd33
Kyaw Thet Paing <capital.kyawthetpaing@gmail.com>
2021-04-22 11:25:56 +0630

    ktp

--- BL/IdouNyuuryokuBL.cs
+++ BL/IdouNyuuryokuBL.cs
@@ -83,6 +83,7 @@ namespace BL
         public string IdouNyuuryoku_CUD(string mode, string xml_header, string xml_detail)
         {
             CKMDL ckmdl = new CKMDL();
+            ckmdl.UseTran = true;
             var parameters = new SqlParameter[3];
             parameters[0] = new SqlParameter("@Mode", SqlDbType.VarChar) { Value = mode };
             parameters[1] = new SqlParameter("@XML_Header", SqlDbType.Xml) { Value = xml_header };
